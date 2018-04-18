b0045b43924307d6ce214a76128ff49aeca34eec
kwon.yuchan <kwon.yuchan@nbplus.kr>
2018-04-18 11:26:23 +0900

    css

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,14 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'nga-login',
-  template: `
-    <h1 class="home">Please login here</h1>
-  `,
-  styles: [`
-    .home {
-      background: greenyellow;
-    }
-  `]
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
 
